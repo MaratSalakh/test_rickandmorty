7b771f0d09c249532a2e4ed92af2992fcbac9dc4
Marat Salakh <marat.salakh.dev@gmail.com>
2024-06-05 12:03:11 +0200

    add fetching data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+<template>
+  <div class="app">
+    <CardsGrid>
+      <Card :card="card" v-for="card in cards"></Card>
+    </CardsGrid>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import CardsGrid from "./components/CardsGrid.vue";
+import Card from "./components/UI/Card.vue";
+
+export default {
+  components: { Card, CardsGrid },
+  data() {
+    return { cards: [] };
+  },
+  methods: {
+    async fetchCards() {
+      try {
+        const response = await axios.get(
+          "https://rickandmortyapi.com/api/character"
+        );
+        console.log(response);
+        this.cards = response.data.results;
+      } catch (e) {
+        alert("Error");
+      }
+    },
+  },
+  mounted() {
+    this.fetchCards();
+  },
+};
 </script>
 
-<template></template>
-
-<style scoped></style>
+<style scoped>
+.app {
+  background-color: rgb(39, 43, 51);
+}
+</style>
