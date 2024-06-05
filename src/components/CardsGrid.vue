0522b6bf200050493e16abbd30045acb951bcb01
Marat Salakh <marat.salakh.dev@gmail.com>
2024-06-05 12:03:27 +0200

    create grid

---
+++ src/components/CardsGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="title">The Rick and Morty</h1>
+  <div class="cardGrid"><slot></slot></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.cardGrid {
+  display: grid;
+  grid-template-columns: 45vw 45vw;
+  justify-content: space-evenly;
+}
+.title {
+  margin-left: 10px;
+  color: white;
+}
+</style>
